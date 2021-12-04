1c6cf7f55e9f82f4e97fbe67dc82859247287bbd
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-12-04 12:29:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,6 +30,12 @@ var ball = {
 //define a variable to hold the status of the game
 game_status = "";
 
+//define preload() function and load the audio files
+function preload(){
+  touched = loadSound("ball_touch_paddel.wav");
+  missed = loadSound("missed.wav");
+}
+
 function setup(){
   var canvas =  createCanvas(700,600);
 
@@ -101,7 +107,7 @@ function draw(){
       fill(250,0,0);
        stroke(0,0,250);
        strokeWeight(0.5);
-      paddle1Y = mouseY; 
+      paddle1Y = rightWristY; 
       rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
       
       
@@ -175,11 +181,13 @@ function move(){
   if (ball.x-2.5*ball.r/2< 0){
   if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
     ball.dx = -ball.dx+0.5; 
+    touched.play();
   }
   else{
     pcscore++;
     reset();
     navigator.vibrate(100);
+    missed.play();
   }
 }
 if(pcscore ==4){
@@ -190,7 +198,7 @@ if(pcscore ==4){
     stroke("white");
     textSize(25)
     text("Game Over!☹☹",width/2,height/2);
-    text("Reload The Page!",width/2,height/2+30)
+    text("Press Restart Button to Play Again",width/2,height/2+30);
     noLoop();
     pcscore = 0;
 }
@@ -219,4 +227,11 @@ function paddleInCanvas(){
   if(mouseY < 0){
     mouseY =0;
   }  
+}
+
+//define restart() function
+function restart(){
+  pcscore = 0;
+  playerscore = 0;
+  loop();
 }
